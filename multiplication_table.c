5ec74d705b583c21622aa991b0c61e97e5e55e23
Vitalino Pimenta <vitalinx7@gmail.com>
2023-07-23 18:20:54 -0400

    added descriptions

--- multiplication_table.c
+++ multiplication_table.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+//this multiplication table shows 1-10 multiplication
 
-int main(){
+int main(){       //main with the variables integers
     int i, j;
     int table = 10;
     int max = 10;
 
-    for (i = 1; i <= table; i++) {
-        for (j=0; j <= max; j++) {
-            printf("%d x %d = %d\n", i, j, i*j);
+    for (i = 1; i <= table; i++) {    //loop for increment
+        for (j=0; j <= max; j++) {    //the limit loop for each table
+            printf("%d x %d = %d\n", i, j, i*j);       //print for each table formated
         }
         printf("\n"); //blank lines between tables
 
